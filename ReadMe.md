9afca026aa233ccf65f42c70c851da0572bade23
JustAnotherHermit <42230158+YahikoSV@users.noreply.github.com>
2020-11-27 14:53:09 +0800

    Add files via upload
    Version 2.1.1

---
+++ ReadMe.md
@@ -0,0 +1,75 @@
+Title: Booth Shoe Bot
+Author: YahikoSV
+Contributor: CrugarMan
+Version: 2.1.1
+
+### FOR PERSONAL USE ONLY: ###
+### ASK PERMISSION FROM AUTHOR IF YOU WANT TO USE IT ###
+
+This is a Shoe Bot for Booth. Since it is only for personal project,
+this only uses one method of purchasing an item. No UI Yet.
+
+Pre-requisites: More necessary info/examples below:
+[1] Has existing Pixiv Account
+[2] Link of the merchandise from the circle/owner.
+[3] Selenium Webdriver (Chrome, Edge, Firefox)
+[4] Payment method must be Paypal and is already completed beforehand (first field)
+[5] Shipping address is already completed beforehand
+[6] Shipping method is either "Physical" or "Digital" only.
+[7] The language of the booth website when last logged-in should be in English.
+[8] Purchases only "1 item with 1 quantity" at a time.
+
+
+# [1] Has existing Pixiv Account
+Basically you need your Username/Email and Password.
+Note: This program do not save data! Data is only stored locally.
+
+# [2] Link of the merchandise from the circle/owner
+There are two similar links for example:
+
+[A] https://booth.pm/en/items/2441613
+[B] https://hololive.booth.pm/items/2441613
+
+The merch is the same but the pages are different. Only the link with the circle/owner (e.g. hololive) domain present (B) can be used.
+
+[3] Selenium Webdriver (Chrome, Edge, Firefox)
+Download Selenium Driver of your browser choice. Make sure it is the same version as your browser.
+Note: TBH, any browser available by selenium should work. 
+
+[6] For Physical (direct) shipping your address must be in Japan either by proxy or in real life. "Buyee" (worldwide shipping) does not work because uses a different method. On the other hand, "Digital" items do not need a shipping address.
+
+Note: Not all bugs and exceptions are all accounted for. There is still a need of human intervention if random bugs do occur.
+
+Note2: There's still a lot of pre-requisites required...
+
+Examples of some bugs:
+[1] Random capcha when logging-in Pixiv. 
+Solution: Solve the capcha by yourselves.
+
+[2] Account failed to log-in properly. 
+Possible Solution [1] Wrong credentials
+Possible Solution [2] The loading when logging-in got overwritten by the next code. Requires adding buffer time
+
+[3] A certain input/click keeps retrying but failed.
+Possible Solution [1] Depends on the speed of your internet, the code may run faster than the loading time. Just wait for it to load.
+Possible Solution [2] If [1] fails. Manually click the button. It should proceed to the next code.
+Possible Solution [3] If [2] fails, Increase your Loadtime_buffer. Your PC/ISP might be too slow such that some codes get overrided.
+
+### IF THERE ARE ANY ISSUES, PLEASE CONTACT US###
+### FEEDBACK IS HIGHLY APPRECIATED ###
+
+Short-term Developments:
+[1] Translate variant type from Japanese to English
+[2] Remove pre-requisite [7]
+[3] Find more bugs/ exceptions
+
+Long-term Developments: 
+[1] Develop User Interface
+
+Wasshoi!
+
+Versions:
+2.1.1 - Bug Fixes and Bug fixes
+2.1 - Refactor code (CrugarMan)
+2.0 - Convert Hololive Shoe Bot -> Booth Shoe Bot
+1.0 - Base Code
---
+++ shoemod.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 21:20:00 2020
+
+@author: YahikoSV
+"""
+
+import datetime as dt
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import requests
+import time
+import sys
+from enum import Enum
+
+class Consts(Enum):
+    MINUTE_LAG = 1
+    SECOND_LAG = 59
+    RELOAD_LAG = 1
+    LOADING_LAG = 5 
+    COUNTDOWN_TIMER = 10 #mins
+
+class ShoeMod:
+    def __init__(self, Pixiv_ID, Pixiv_Password, webdriver_type, webdriver_path, latency = 1):
+        self.Pixiv_ID = Pixiv_ID
+        self.Pixiv_Password = Pixiv_Password
+        self.webdriver_type = webdriver_type
+        self.webdriver_path = webdriver_path
+        self.latency = latency
+        self.driver = None
+        self.market_URL = None
+
+    def LogInBooth(self):
+        url = "https://booth.pm/en" #force cache into EN
+        if self.webdriver_type == "Chrome":
+            self.driver = webdriver.Chrome(executable_path = self.webdriver_path)
+            self.driver.get(url)
+        elif self.webdriver_type == "Edge":
+            self.driver = webdriver.Edge(executable_path = self.webdriver_path)
+            self.driver.get(url)
+        elif self.webdriver_type == "Firefox":
+            self.driver = webdriver.Firefox(executable_path = self.webdriver_path)
+            self.driver.get(url)
+        else:
+            print("Invalid Webdriver")
+            sys.exit()
+
+        self.LoadingBuffertoClick('//a[@href="/users/sign_in"]','Waiting...','2/3 Pixiv SignIn Opened')
+        print("1/3 Booth SignIn Opened")
+
+        self.LoadingBuffertoClick('//a[@class="btn pixiv-oauth signin full-length"]','Waiting...','2/3 Pixiv SignIn Opened')
+
+        self.LoadingBuffertoType('//input[@autocomplete="username"]','Typing...','ID Typed Sucessfully', self.Pixiv_ID)
+        self.LoadingBuffertoType('//input[@autocomplete="current-password"]','Typing...','Password Typed Sucessfully', self.Pixiv_Password)
+
+        #for the Privacy Policy Update March 2020
+        try:
+            self.driver.find_element_by_xpath('//button[@class="styled-button"]').click()
+        except:
+            pass
+
+        SignUp = self.driver.find_elements_by_xpath('//button[@class="signup-form__submit"]')[1]
+        self.LoadingBuffertoClickSimple(SignUp,'Loading or need manual guidance','3/3 SignIn Successful')
+
+    def LogOutBooth(self):
+        url = "https://booth.pm/"
+        self.driver.get(url)
+        self.LoadingBuffertoClick('//span[@class="cmd-label"]','Waiting...','1/2 Tab Opened')
+        self.LoadingBuffertoClick('//a[@href="/users/sign_out"]','Waiting...','2/2 Logged-Out Successfully')
+
+    def Preparation(self, URL, PrePurchase, Time_Available):
+        self.driver.get(URL)
+        Time = [int(t) for t in Time_Available.split(':')]
+        if PrePurchase == True:
+            Current_Time = None
+            print("waiting...")
+            print (Time_Available)
+            while [Time[0], Time[1]] >= [dt.datetime.now().hour,  dt.datetime.now().minute + Consts.COUNTDOWN_TIMER.value]:
+                continue
+
+            print("countdown!")
+            while(Current_Time := dt.datetime.now()).strftime('%H:%M') != Time_Available:
+                time.sleep(Consts.RELOAD_LAG.value)
+                print("{:02d}:{:02d}".format(int(Time[1]) - Current_Time.minute - Consts.MINUTE_LAG.value, Consts.SECOND_LAG.value - Current_Time.second), end = '\r')
+
+        print('')
+        print("VIVA HOLOLIVE RESISTANCE")
+
+    def AddToCart(self, Button_Rank, Digital_Only):
+        self.driver.refresh() #this is where it might take a while
+
+        while True:
+            try:
+                self.driver.find_elements_by_xpath('//button')[Button_Rank].click()
+                print('1/5 Item Selected')
+                break
+            except:
+                print('loading...')
+                time.sleep(self.latency)
+
+        self.market_URL = self.driver.current_url
+
+        self.LoadingBuffertoClick('//a[@class="btn btn--primary u-w-sp-100"]','Checkout Loading...', '2/5 Checkout Selected')
+
+        if Digital_Only == True:
+            self.LoadingBuffertoClick('//input[@value="Order Confirmation"]','Payment Type Loading...','3/5 Payment Type Selected')
+            print('4/5 Shipping Type Skipped')
+        else:
+            self.LoadingBuffertoClick('//input[@value="Next Step"]','Payment Type Loading...','3/5 Payment Type Selected')
+            self.LoadingBuffertoClick('//button[@class="btn primary-color mobile-full-length"]','Shipping Loading...','4/5 Shipping Type Selected')
+
+    def PurchaseItem(self, Ready_to_Purchase):
+        if Ready_to_Purchase == True:
+            #Failsafe
+            #self.LoadingBuffertoClick('//input[@value="Place Order"]','Placing Order...', '5/5 Order Completed')
+            now = dt.datetime.now()
+            print(f' Order Completed: {now.strftime("%H:%M:%S")}')
+            logout = input("log-out? (type 'y' and Enter):")
+            if logout == 'y':
+                self.LogOutBooth()
+            print('Logged-out')
+        else:
+            now = dt.datetime.now()
+            print(f' Time Completed: {now.strftime("%H:%M:%S")}')
+            clearpurchase = input("Clear purchase and log-out? (type 'y' and Enter):")
+            if clearpurchase == 'y':
+                self.ClearPurchase()
+                self.LogOutBooth()
+                print('Purchase Cleared and Logged-out')
+
+    def ClearPurchase(self):
+        self.driver.get(self.market_URL)
+        self.LoadingBuffertoClick('//i[@class="icon-cancel bigger no-margin"]','Manually Approve Removal...','1/1 Purchase Cleared')
+        input("Press Enter if you removed the confirmation message. ")
+
+    def LoadingBuffertoClick(self, elementname, fail_msg, success_msg):
+        while True:
+            try:
+                self.driver.find_element_by_xpath(elementname).click()
+                print(success_msg)
+                break
+            except Exception as e:
+                print(fail_msg)
+                #print(f"Error: {str(e)}") #if you want to show error msg 
+                time.sleep(self.latency)
+                continue
+
+    def LoadingBuffertoClickSimple(self, element, fail_msg, success_msg):
+        while True:
+            try:
+                element.click()
+                print(success_msg)
+                break
+            except Exception as e:
+                print(fail_msg)
+                #print(f"Error: {str(e)}") #if you want to show error msg 
+                time.sleep(self.latency)
+                continue
+
+    def LoadingBuffertoType(self, elementname, fail_msg, success_msg, credentials):
+        while True:
+            try:
+                self.driver.find_element_by_xpath(elementname).send_keys(credentials)
+                print(success_msg)
+                break
+            except:
+                print(fail_msg)
+                time.sleep(self.latency)
+                continue
+
+    def FindProductVariation(self, URL, PrePurchase):
+        source = requests.get(URL).text
+        soup = bs(source, 'lxml')
+
+        info = soup.find_all('li', class_="variation-item")
+        print(' Available Items:')
+        print('')
+
+        rank = 0
+        if PrePurchase == True:
+            for variation in info:
+                rank = self.InputasAvailable(variation,rank)
+        elif PrePurchase == False:
+            for variation in info:
+                if bool(variation.find('form')) == True:
+                    rank = self.InputasAvailable(variation,rank)
+        else:
+            print('Invalid Prepurchase Value')
+            sys.exit()
+
+    def InputasAvailable(self, variation, rank):
+        if name_exists := (variation.find('div', class_="variation-name").contents != []):
+            variation_name = variation.find('div', class_="name").text
+        variation_price = variation.find('div', class_="variation-price").text
+        variation_type = variation.find('span', class_="type").text
+
+        rank += 1
+
+        print(f' Button Rank = {rank}')
+        if variation_type == 'ダウンロード商品':
+            print(f' Type of Product = {variation_type}  (Digital Only)')
+        else:    
+            print(f' Type of Product = {variation_type}')
+            
+        print(f' Price = {variation_price}')
+        if name_exists:
+            print(f' Variation Name = {variation_name}')
+        print('')
+        return rank
+
+    def Run(self, URL, PrePurchase, Time_Available = None, Wallet_check = True):
+        ### 1. Check for Available Items and find respective Button Rank
+        self.FindProductVariation(URL, PrePurchase)
+        Button_Rank = int(input("Enter Button Rank: "))
+        
+        ### 2. Ask Permissions ###
+        Digital_Only = False
+        if input("Product is Digital Only? (y/n): ") == 'y':
+            Digital_Only = True           
+        if Wallet_check:
+            Ready_to_Purchase = (input("Warning: typing y will purchase the item: Purchase Item? (y/n): ") == 'y')          
+        if Ready_to_Purchase == False:
+            if input("Do you want to simulate the purchase? (y/n): ") != 'y':
+                sys.exit()
+                
+        ### 3. Purchase the item ###
+        self.LogInBooth()
+        if PrePurchase and Time_Available == None:
+            Time_Available = input("Enter purchase time \"HH:MM\" (military): ")
+        else:
+            time.sleep(Consts.LOADING_LAG.value)
+        if PrePurchase == True:
+            self.Preparation(URL,PrePurchase, Time_Available)
+        else:
+            self.driver.get(URL)
+            print("VIVA HOLOLIVE RESISTANCE")
+        self.AddToCart(Button_Rank, Digital_Only)
+
+        self.PurchaseItem(Ready_to_Purchase)
+        if input("Close Window? (y/n): ") == 'y':
+            self.driver.close()
